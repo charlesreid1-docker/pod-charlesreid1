e17712d31fa0331bbe8e33e694ec704e2a426ca4
Charles Reid <charlesreid1@gmail.com>
2018-03-30 01:22:33 -0700

    finalize gitea utils

--- utils-gitea/backup_gitea.sh
+++ utils-gitea/backup_gitea.sh
@@ -4,13 +4,20 @@ function usage {
     echo ""
     echo "backup_gitea.sh script:"
     echo "Run a gitea dump from the gitea container,"
-    echo "and put the results into a target zip file."
+    echo "and back up the gitea avatars."
+    echo "Gitea backups are dumped to gitea-dump-000000.zip"
+    echo "and gitea-avatars.zip and copied to the target directory."
     echo ""
-    echo "       ./backup_gitea.sh <zip-file>"
+    echo "       ./backup_gitea.sh <target-dir>"
     echo ""
     echo "Example:"
     echo ""
-    echo "       ./backup_gitea.sh /path/to/gitea.zip"
+    echo "       ./backup_gitea.sh /path/to/backups/"
+    echo ""
+    echo "creates the files:"
+    echo""
+    echo "      /path/to/backups/gitea-dump-000000.zip"
+    echo "      /path/to/backups/gitea-avatars.zip"
     echo ""
     echo ""
     exit 1;
@@ -19,11 +26,22 @@ function usage {
 if [[ "$#" -eq 1 ]];
 then
 
-    docker exec -it dgitea_server_1 /bin/bash -c 'mkdir /backup && cd /backup && /app/gitea/gitea dump'
+    # create a backup target
+    docker exec -it dgitea_server_1 /bin/bash -c 'mkdir /backup'
+    
+    # create the backup zip files
+    docker exec -it dgitea_server_1 /bin/bash -c 'cd /backup && /app/gitea/gitea dump'
+    docker exec -it dgitea_server_1 /bin/bash -c 'm/data/gitea/ && zip /backup/gitea-avatars.zip avatars'
+
+    # copy the backup dir, containing the zip files, to .
     docker cp dgitea_server_1:/backup .
+
+    # clean up in the container
     docker exec -it dgitea_server_1 /bin/bash -c 'rm -rf /backup'
-    mv backup/*.zip $1
-    rm -r backup/
+
+    # todo: check if $1 is a directory. 
+    # if not, at least stuff will still be left at backup/
+    mkdir -p $1 && mv backup/*.zip $1/. && rm -rf backup
 
 else
     usage
--- utils-gitea/restore_gitea.sh
+++ utils-gitea/restore_gitea.sh
@@ -9,13 +9,13 @@ function usage {
     echo ""
     echo "Example:"
     echo ""
-    echo "       ./restore_gitea.sh /path/to/gitea.zip"
+    echo "       ./restore_gitea.sh /path/to/gitea.zip /path/to/gitea-avatars.zip"
     echo ""
     echo ""
     exit 1;
 }
 
-if [[ "$#" -eq 1 ]];
+if [[ "$#" -eq 2 ]];
 then
 
     EXEC="docker exec -it dgitea_server_1"
@@ -23,22 +23,27 @@ then
 
     echo "Copying files into container"
     ${EXEC} /bin/bash -c 'mkdir /restore'
-    ${CP} $1 dgitea_server_1:/restore/.
+    ${CP} $1 dgitea_server_1:/restore/gitea-dump.zip
+    ${CP} $2 dgitea_server_1:/restore/gitea-avatars.zip
 
     echo "Unpacking files inside container"
-    ${EXEC} /bin/bash -c 'unzip -qq /restore/*.zip -d /restore'
+    ${EXEC} /bin/bash -c 'unzip -qq /restore/gitea-dump.zip -d /restore'
+    ${EXEC} /bin/bash -c 'unzip -qq /restore/gitea-avatars.zip -d /restore'
 
     echo "Unpacking repositories inside container"
     ${EXEC} /bin/bash -c 'unzip -qq /restore/gitea-repo.zip -d /restore'
 
-    echo "Restoring 1/3: repositories"
+    echo "Restoring 1/4: repositories"
     ${EXEC} /bin/bash -c 'rm -rf /data/git/repositories && cp -r /restore/repositories /data/git/repositories'
 
-    echo "Restoring 2/3: custom files"
+    echo "Restoring 2/4: custom files"
     ${EXEC} /bin/bash -c 'rm -rf /data/gitea && cp -r /restore/custom /data/gitea'
 
-    echo "Restoring 3/3: sqlite database"
+    echo "Restoring 3/4: sqlite database"
     ${EXEC} /bin/bash -c 'cat /restore/gitea-db.sql | sed "s/false/0/g" | sed "s/true/1/g" | sqlite3 /data/gitea/gitea.db'
+
+    echo "Restoring 4/4: avatars"
+    ${EXEC} /bin/bash -c 'rm -rf /data/gitea/avatars && cp -r /restore/avatars /data/gitea/avatars'
 
     echo "Cleaning up"
     ${EXEC} /bin/bash -c 'rm -rf /restore'
