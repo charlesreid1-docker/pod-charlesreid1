4ca16c8d6f6a2a54ba0a3bdba002540e248129e5
Charles Reid <charlesreid1@gmail.com>
2018-04-01 13:11:53 -0700

    skip restoration of custom path, everything is under vc now

--- utils-gitea/backup_gitea.sh
+++ utils-gitea/backup_gitea.sh
@@ -23,24 +23,32 @@ function usage {
     exit 1;
 }
 
+echo ""
+echo "Backup Gitea:"
+echo "----------------"
+echo ""
+
+NAME="podcharlesreid1_stormy_gitea_1"
+
 if [[ "$#" -eq 1 ]];
 then
 
-    # create a backup target
-    docker exec -it dgitea_server_1 /bin/bash -c 'mkdir /backup'
+    echo " - Creating backup target"
+    docker exec -it $NAME /bin/bash -c 'mkdir /backup'
     
-    # create the backup zip files
-    docker exec -it dgitea_server_1 /bin/bash -c 'cd /backup && /app/gitea/gitea dump'
-    docker exec -it dgitea_server_1 /bin/bash -c 'm/data/gitea/ && zip /backup/gitea-avatars.zip avatars'
+    echo " - Creating backup zip files"
+    docker exec -it $NAME /bin/bash -c 'cd /backup && /app/gitea/gitea dump'
+    docker exec -it $NAME /bin/bash -c 'm/data/gitea/ && zip /backup/gitea-avatars.zip avatars'
 
-    # copy the backup dir, containing the zip files, to .
-    docker cp dgitea_server_1:/backup .
+    echo " - Copying backup directory (with zip files) to ."
+    docker cp $NAME:/backup .
 
-    # clean up in the container
-    docker exec -it dgitea_server_1 /bin/bash -c 'rm -rf /backup'
+    echo " - Cleaning up container"
+    docker exec -it $NAME /bin/bash -c 'rm -rf /backup'
 
     # todo: check if $1 is a directory. 
     # if not, at least stuff will still be left at backup/
+    echo " - Copy zip backup to target"
     mkdir -p $1 && mv backup/*.zip $1/. && rm -rf backup
 
 else
--- utils-gitea/restore_gitea.sh
+++ utils-gitea/restore_gitea.sh
@@ -15,40 +15,49 @@ function usage {
     exit 1;
 }
 
+echo ""
+echo "Restore Gitea:"
+echo "----------------"
+echo ""
+
+NAME="podcharlesreid1_stormy_gitea_1"
+
 if [[ "$#" -eq 2 ]];
 then
 
-    EXEC="docker exec -it dgitea_server_1"
+    EXEC="docker exec -it $NAME"
     CP="docker cp"
 
-    echo "Copying files into container"
+    echo "- Copying files into container"
     ${EXEC} /bin/bash -c 'mkdir /restore'
-    ${CP} $1 dgitea_server_1:/restore/gitea-dump.zip
-    ${CP} $2 dgitea_server_1:/restore/gitea-avatars.zip
+    ${CP} $1 $NAME:/restore/gitea-dump.zip
+    ${CP} $2 $NAME:/restore/gitea-avatars.zip
 
-    echo "Unpacking files inside container"
+    echo "- Unpacking files inside container"
     ${EXEC} /bin/bash -c 'unzip -qq /restore/gitea-dump.zip -d /restore'
     ${EXEC} /bin/bash -c 'unzip -qq /restore/gitea-avatars.zip -d /restore'
 
-    echo "Unpacking repositories inside container"
+    echo " - Unpacking repositories inside container"
     ${EXEC} /bin/bash -c 'unzip -qq /restore/gitea-repo.zip -d /restore'
 
-    echo "Restoring 1/4: repositories"
+    echo " - Restoring 1/4: repositories"
     ${EXEC} /bin/bash -c 'rm -rf /data/git/repositories && cp -r /restore/repositories /data/git/repositories'
 
-    echo "Restoring 2/4: custom files"
-    ${EXEC} /bin/bash -c 'rm -rf /data/gitea && cp -r /restore/custom /data/gitea'
+    # We are actually just gonna skip this whole step,
+    # since everything here should be in d-gitea repo
+    echo " - Restoring 2/4: custom files    ...skipping.."
+    #${EXEC} /bin/bash -c 'rm -rf /data/gitea && cp -r /restore/custom /data/gitea'
 
-    echo "Restoring 3/4: sqlite database"
+    echo " - Restoring 3/4: sqlite database"
     ${EXEC} /bin/bash -c 'cat /restore/gitea-db.sql | sed "s/false/0/g" | sed "s/true/1/g" | sqlite3 /data/gitea/gitea.db'
 
-    echo "Restoring 4/4: avatars"
+    echo " - Restoring 4/4: avatars"
     ${EXEC} /bin/bash -c 'rm -rf /data/gitea/avatars && cp -r /restore/avatars /data/gitea/avatars'
 
-    echo "Moving old app.ini"
+    echo " - Moving old app.ini"
     ${EXEC} /bin/bash -c 'mv /data/gitea/conf/app.ini /data/gitea/conf/app.ini.old'
 
-    echo "Cleaning up"
+    echo " - Cleaning up"
     ${EXEC} /bin/bash -c 'rm -rf /restore'
 
 else
