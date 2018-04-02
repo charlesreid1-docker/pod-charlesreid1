cb7fda439df731812fe93df307558ba47fb9c213
Charles Reid <charlesreid1@gmail.com>
2018-04-02 03:28:43 -0700

    add a fifth step to restore script: fixing hook locations.

--- utils-gitea/restore_gitea.sh
+++ utils-gitea/restore_gitea.sh
@@ -40,22 +40,36 @@ then
     echo " - Unpacking repositories inside container"
     ${EXEC} /bin/bash -c 'unzip -qq /restore/gitea-repo.zip -d /restore'
 
-    echo " - Restoring 1/4: repositories"
+    echo " - Restoring 1/5: repositories"
     ${EXEC} /bin/bash -c 'rm -rf /data/git/repositories && cp -r /restore/repositories /data/git/repositories'
 
     # We are actually just gonna skip this whole step,
     # since everything here should be in d-gitea repo
-    echo " - Restoring 2/4: custom files    ...skipping.."
+    echo " - Restoring 2/5: (skipping custom files)"
+    #echo " - Restoring 2/5: custom files"
+    #echo "     - Moving old app.ini"
+    #${EXEC} /bin/bash -c 'mv /data/gitea/conf/app.ini /data/gitea/conf/app.ini.old'
+    #echo "     - Restoring custom files"
     #${EXEC} /bin/bash -c 'rm -rf /data/gitea && cp -r /restore/custom /data/gitea'
 
-    echo " - Restoring 3/4: sqlite database"
+    echo " - Restoring 3/5: sqlite database"
     ${EXEC} /bin/bash -c 'cat /restore/gitea-db.sql | sed "s/false/0/g" | sed "s/true/1/g" | sqlite3 /data/gitea/gitea.db'
 
-    echo " - Restoring 4/4: avatars"
+    echo " - Restoring 4/5: avatars"
     ${EXEC} /bin/bash -c 'rm -rf /data/gitea/avatars && cp -r /restore/avatars /data/gitea/avatars'
 
-    echo " - Moving old app.ini"
-    ${EXEC} /bin/bash -c 'mv /data/gitea/conf/app.ini /data/gitea/conf/app.ini.old'
+    echo " - Restoring 5/5: repairing paths in .git/hooks"
+    ###################
+    # NOTE: This is entirely case-dependent.
+    #       If backup/restore happened from same gitea dir structure,
+    #       this section should be removed entirely.
+    #
+    #       Example below swaps out /www/gitea with /data/gitea
+    #
+    #${EXEC} /bin/bash -c 'find /data/git/repositories -type f -exec sed -i -e "s%/www/gitea/bin/custom/conf%/data/gitea/conf%g" {} \;'
+    #${EXEC} /bin/bash -c 'find /data/git/repositories -type f -exec sed -i -e "s%/www/gitea/bin/gitea%/app/data/gitea%g" {} \;'
+    #
+    ###################
 
     echo " - Cleaning up"
     ${EXEC} /bin/bash -c 'rm -rf /restore'
