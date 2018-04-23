c3b9eeb60424f3122f75ac974fa6ee694e58ef42
Charles Reid <charlesreid1@gmail.com>
2018-04-23 03:45:40 -0700

    update update submodule script

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-for dir in `find * -maxdepth 0 -type d`; do
-    ## Option 1: tedious
-    #(
-    #cd $dir
-    #git checkout master
-    #git pull origin master
-    #)
-
-    ## Option 2: slick
-    #git submodule update --init --remote $dir
-
-    # Option 3: best
-    git submodule update --init --recursive
+for dir in `/bin/ls -1 | /bin/grep "d-"`; do
+    cd $dir
+    git checkout master
+    git pull origin master
+    cd ..
 done
 
 git commit -a -m 'Updating submodules to latest'
