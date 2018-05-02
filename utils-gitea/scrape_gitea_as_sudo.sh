25803aa6706f9177fa2b558a928b4c4de2ebd1fd
Charles Reid <charlesreid1@gmail.com>
2018-05-02 01:08:20 -0700

    fix directories in sudo scrape gitea script

--- utils-gitea/scrape_gitea_as_sudo.sh
+++ utils-gitea/scrape_gitea_as_sudo.sh
@@ -9,47 +9,66 @@
 # It commits the new commit count data
 # to https://git.charlesreid1.com/data/charlesreid1-data
 
+if [ "$(id -u)" != "0" ]; then
+    echo ""
+    echo ""
+    echo "This script should be run as root."
+    echo ""
+    echo ""
+    exit 1;
+fi
+
 WORKDIR="/tmp/gitea-temp"
-mkdir -p $WORDIR && cd $WORKDIR
+GITDIR="/tmp/gitea-temp/charlesreid1-data"
+
+rm -rf ${WORKDIR}
+mkdir -p ${WORKDIR} 
+sudo chown -R charles:charles ${WORKDIR}
 
 
 # Step 1: clone repo
 
-sudo -H -u charles git clone https://git.charlesreid1.com/data/charlesreid1-data.git
-cd charlesreid1-data
+sudo -H -u charles git clone ssh://git@git.charlesreid1.com:222/data/charlesreid1-data.git ${GITDIR}
 
 
 # Step 2: extract commit dates
 
-rm -f ${WORKDIR}/commit_dates
+rm -f ${GITDIR}/commit_dates
+
 for dir in `find /var/lib/docker/volumes/podcharlesreid1_stormy_gitea_data/_data/git/repositories -mindepth 2 -maxdepth 2 -type d`; do
     git --git-dir=$dir --work-tree=${WORKDIR} \
         log \
         --all --author="harles" --oneline --pretty="%H %ai" \
-        | cut -f 2 -d " " >> commit_dates
+        | cut -f 2 -d " " >> ${GITDIR}/commit_dates
 done
 
 
 # Step 3: bin commit dates
 
-words=$( cat commit_dates )
+words=$( cat ${GITDIR}/commit_dates )
+
+echo "date,commits" > ${GITDIR}/commit_counts.csv
 
 echo $words | sort | python -c 'import sys; 
 from collections import Counter; c=Counter(sys.stdin.read().strip().split(" "));
-print("\n".join(("%s, %s"%(k, c[k]) for k in c.keys())));' | sort > ${WORKDIR}/commit_counts.csv
+print("\n".join(("%s,%s"%(k, c[k]) for k in c.keys())));' | sort | awk 'NF' >> ${GITDIR}/commit_counts.csv
 
-rm -f commit_dates
+rm -f ${GITDIR}/commit_dates
+chown charles:charles ${GITDIR}/commit_counts.csv
+
 
 
 # Step 4: Commit New Data
 
+(
+cd ${GITDIR}
 sudo -H -u charles git add commit_counts.csv
 sudo -H -u charles git commit commit_counts.csv -m '[scrape_gitea_as_sudo.sh] updating git commit count data'
 sudo -H -u charles git push origin master
+)
 
 
 # Step 5: Clean Up
 
-cd /tmp
-rm -rf $WORKDIR
+sudo rm -rf ${WORKDIR}
 
