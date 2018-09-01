9e80ccbec4f7b47dcd926643299f10f592205591
Charles Reid <charlesreid1@gmail.com>
2018-09-01 09:23:50 -0700

    add change pw util

---
+++ utils-mw/change_passwd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# change the password for the Admin user.
+
+function usage {
+    echo ""
+    echo "change_passwd.sh script:"
+    echo "This changes the password for the Admin user."
+    echo ""
+    echo "       ./change_passwd.sh <password>"
+    echo ""
+    echo "Inside the container it runs the "
+    echo "changePassword.php script included "
+    echo "with MediaWiki."
+    echo ""
+    exit 1;
+}
+
+if [[ "$#" -eq 0 ]];
+then
+
+    NAME="podcharlesreid1_stormy_mw_1"
+    docker exec -it ${NAME} ls /var/www/html/maintenance/changePassword.php --user="Anna Graham"
+
+else
+    usage
+fi
