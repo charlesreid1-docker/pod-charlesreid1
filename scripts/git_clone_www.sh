3718ba0add75b9ed644077345f53afc12bdcca37
C Reid <charlesreid1@Gmail.com>
2019-03-23 01:00:55 -0700

    update git clone/pull scripts for /www to do all domains

--- scripts/git_clone_www.sh
+++ scripts/git_clone_www.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-REPOURL="https://git.charlesreid1.com/charlesreid1/charlesreid1.com.git"
+declare -a DOMAINS=("charlesreid1.com" "charlesreid1.blue" "charlesreid1.red")
 
-git -C /www/charlesreid1.com \
-    clone \
-    --separate-git-dir=git \
-    -b gh-pages \
-    $REPOURL htdocs
+for DOMAIN in "${DOMAINS[@]}"; do
+
+    ## git.charlesreid1.com (probably not available):
+    #REPOURL="https://git.charlesreid1.com/charlesreid1/${DOMAIN}.git"
+
+    # github.com:
+    REPOURL="https://github.com/charlesreid1-docker/${DOMAIN}.git"
+
+    echo "Cloning repo for ${DOMAIN} to /wwww"
+
+    git -C /www/${DOMAIN} \
+        clone \
+        --separate-git-dir=git \
+        -b gh-pages \
+        $REPOURL htdocs
+
+done
 
--- scripts/git_pull_www.sh
+++ scripts/git_pull_www.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git -C /www/charlesreid1.com \
-    --git-dir=git --work-tree=htdocs \
-    pull origin gh-pages
+declare -a DOMAINS=("charlesreid1.com" "charlesreid1.blue" "charlesreid1.red")
 
+for DOMAIN in "${DOMAINS[@]}"; do
+
+    echo "Cloning repo for ${DOMAIN} to /wwww"
+
+    git -C /www/${DOMAIN} \
+        --git-dir=git --work-tree=htdocs \
+        pull origin gh-pages
+
+done
