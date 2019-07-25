e1330531ff335b68293bb1aba58c504d646c8d49
Charles Reid <charlesreid1@gmail.com>
2019-07-25 14:33:53 -0700

    improve placement of set x statements in backup gitea script

--- utils-gitea/backup_gitea.sh
+++ utils-gitea/backup_gitea.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 
 function usage {
     set +x
@@ -47,33 +46,51 @@ then
     fi
 
     echo "Step 1: Creating backup target"
+    set -x
     ${DOCKER} $NAME /bin/bash -c 'mkdir /backup'
+    set +x
     
     echo "Step 2: Creating backup zip files:"
+
     echo "     Step 2A: gitea dump zip"
+    set -x
     ${DOCKER} $NAME /bin/bash -c '/app/gitea/gitea dump'
+    set +x
+
     echo "     Step 2B: gitea avatars zip"
+    set -x
     ${DOCKER} $NAME /bin/bash -c 'cd /data/gitea/ && tar czf /backup/gitea-avatars.tar.gz avatars'
+    set +x
 
     echo "Step 3: Moving gitea dump to /backup directory"
+    set -x
     ${DOCKER} $NAME /bin/bash -c 'mv /tmp/gitea-dump-*/* /backup/.'
+    set +x
 
     TEMP_BACKUP=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 
     echo "Step 4: Copying backup directory (with zip files) to backup location $1"
+
     echo "     Step 4A: Making temporary backup location"
     #mkdir -p $TEMP_BACKUP
+
     echo "     Step 4B: Copying /backup directory to temporary backup location $1"
+    set -x
     docker cp $NAME:/backup/* $1/.
+    set +x
 
     TAR_PREFIX="$(echo $V | sed 's+/$++g')"
 
+    set -x
     tar -cvf ${TAR_PREFIX}.tar $1
     rm -fr $1
+    set +x
 
     echo "Step 6: Cleaning up container"
+    set -x
     ${DOCKER} $NAME /bin/bash -c 'rm -rf /backup'
     ${DOCKER} $NAME /bin/bash -c 'rm -rf /tmp/gitea-dump-*'
+    set +x
 
     echo "Step 7: Cleaning up local host"
     #rm -rf $TEMP_BACKUP
