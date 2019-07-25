fe539e1f4f9f733c251a86f997417b2a79a18b65
Charles Reid <charlesreid1@gmail.com>
2019-07-24 18:02:32 -0700

    fix spew in daily backup scripts

--- utils-backup/daily_wikifiles.sh
+++ utils-backup/daily_wikifiles.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 # Just make a daily MediaWiki files backup.
-set -x
 
 stamp="`date +"%Y-%m-%d"`"
 backup_tool="${HOME}/codes/docker/pod-charlesreid1/utils-mw/backup_wikifiles.sh"
@@ -16,18 +15,18 @@ mkdir -p ${backup_dir}
 mkdir -p ${log_dir}
 cat /dev/null > ${log_target}
 
-echo "====================================" >> ${log_target}
-echo "=== MediaWiki Files Backup =========" >> ${log_target}
-echo "====================================" >> ${log_target}
-echo ""                                     >> ${log_target}
-echo "Backup Utility: ${backup_tool}"       >> ${log_target}
-echo "Backup Target: ${backup_target}"      >> ${log_target}
-echo "Log Target: ${log_target}"            >> ${log_target}
-echo ""                                     >> ${log_target}
-echo "Command: ${backup_tool} ${backup_target} >> ${log_target} 2>&1 " >> ${log_target}
-echo "" >> ${log_target}
+echo "====================================" | tee ${log_target}
+echo "=== MediaWiki Files Backup =========" | tee ${log_target}
+echo "====================================" | tee ${log_target}
+echo ""                                     | tee ${log_target}
+echo "Backup Utility: ${backup_tool}"       | tee ${log_target}
+echo "Backup Target: ${backup_target}"      | tee ${log_target}
+echo "Log Target: ${log_target}"            | tee ${log_target}
+echo ""                                     | tee ${log_target}
 
+set -x
 ${backup_tool} ${backup_target} >> ${log_target} 2>&1 
+set +x
 
-echo "Done" >> ${log_target}
+echo "Done" | tee ${log_target}
 
