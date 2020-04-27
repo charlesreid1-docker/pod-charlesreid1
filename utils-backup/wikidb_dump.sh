562f1631545d7aa6cd316530b11f90c700a3944a
Charles Reid <charlesreid1@gmail.com>
2020-04-27 14:06:58 -0700

    add new gitea and wikidb dump files

---
+++ utils-backup/wikidb_dump.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# Run the mysql dump command to back up wikidb table, and send the
+# resulting SQL file to the specified backup directory.
+#
+# Backup directory:
+#       /home/user/backups/mysql
+
+BACKUP_DIR="$HOME/backups/mysql"
+CONTAINER_NAME="pod-charlesreid1_stormy_mysql_1"
+
+function usage {
+    set +x
+    echo ""
+    echo "wikidb_dump.sh script:"
+    echo ""
+    echo "Run the mysql dump command on the wikidb table in the container,"
+    echo "and copy the resulting SQL file to the specified directory."
+    echo "The resulting mysql dump SQL file will be timestamped."
+    echo ""
+    echo "       ./wikidb_dump.sh [BACKUP_DIRECTORY]"
+    echo ""
+    echo "Example:"
+    echo ""
+    echo "       ./wikidb_dump.sh /path/to/backups/"
+    echo "       (creates /path/to/backups/wikidb_20001231_235959.sql)"
+    echo ""
+    exit 1;
+}
+
+if [ "$(id -u)" == "0" ]; then
+    echo ""
+    echo ""
+    echo "This script should NOT be run as root!"
+    echo ""
+    echo ""
+    exit 1;
+fi
+
+if [ "$#" == "1" ]; then
+
+    STAMP="`date +"%Y-%m-%d"`"
+    TARGET="wikidb_${STAMP}.sql"
+
+    echo ""
+    echo "pod-charlesreid1: wikidb_dump.sh"
+    echo "--------------------------------"
+    echo ""
+    echo "Backup target: ${TARGET}"
+    echo ""
+
+    mkdir -p $BACKUP_DIR
+
+    # If this script is being run from a cron job,
+    # don't use -i flag with docker
+    CRON="$( pstree -s $$ | /bin/grep -c cron )"
+    DOCKERX=""
+    if [[ "$CRON" -eq 1 ]]; 
+    then
+        DOCKERX="docker exec -t"
+    else
+        DOCKERX="docker exec -it"
+    fi
+
+    echo "Running mysqldump..."
+    set -x
+    ${DOCKERX} ${CONTAINER_NAME} sh -c 'exec mysqldump wikidb --databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > $TARGET
+    set +x
+
+    echo "Done."
+else
+    usage
+fi
