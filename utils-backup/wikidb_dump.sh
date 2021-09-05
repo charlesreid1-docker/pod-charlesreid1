3d6e20263a7717e68c6bf3a134bcec39d73c7fd7
Charles Reid <charlesreid1@gmail.com>
2021-09-05 16:27:37 -0700

    fix wikidb dump script to match private-wiki wikidb dump script

--- utils-backup/wikidb_dump.sh
+++ utils-backup/wikidb_dump.sh
@@ -6,8 +6,9 @@
 # Backup directory:
 #       /home/user/backups/mysql
 
-BACKUP_DIR="$HOME/backups/mysql"
+BACKUP_DIR="$HOME/backups"
 CONTAINER_NAME="stormy_mysql"
+STAMP="`date +"%Y%m%d"`"
 
 function usage {
     set +x
@@ -16,14 +17,13 @@ function usage {
     echo ""
     echo "Run the mysql dump command on the wikidb table in the container,"
     echo "and copy the resulting SQL file to the specified directory."
-    echo "The resulting mysql dump SQL file will be timestamped."
     echo ""
     echo "       ./wikidb_dump.sh"
     echo ""
     echo "Example:"
     echo ""
     echo "       ./wikidb_dump.sh"
-    echo "       (creates ${BACKUP_DIR}/wikidb_20200101_000000.sql)"
+    echo "       (creates ${BACKUP_DIR}/20200101/wikidb_20200101.sql)"
     echo ""
     exit 1;
 }
@@ -39,22 +39,22 @@ fi
 
 if [ "$#" == "0" ]; then
 
-    STAMP="`date +"%Y-%m-%d"`"
     TARGET="wikidb_${STAMP}.sql"
+    BACKUP_TARGET="${BACKUP_DIR}/${STAMP}/${TARGET}"
 
     echo ""
     echo "pod-charlesreid1: wikidb_dump.sh"
     echo "--------------------------------"
     echo ""
-    echo "Backup target: ${BACKUP_DIR}/${TARGET}"
+    echo "Backup target: ${BACKUP_TARGET}"
     echo ""
 
-    mkdir -p $BACKUP_DIR
+    mkdir -p ${BACKUP_DIR}/${STAMP}
 
     # If this script is being run from a cron job,
     # don't use -i flag with docker
     CRON="$( pstree -s $$ | /bin/grep -c cron )"
-    DOCKER="/usr/bin/docker"
+    DOCKER=$(which docker)
     DOCKERX=""
     if [[ "$CRON" -eq 1 ]]; 
     then
@@ -65,7 +65,7 @@ if [ "$#" == "0" ]; then
 
     echo "Running mysqldump"
     set -x
-    ${DOCKERX} ${CONTAINER_NAME} sh -c 'exec mysqldump wikidb --databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${BACKUP_DIR}/${TARGET}
+    ${DOCKERX} ${CONTAINER_NAME} sh -c 'exec mysqldump wikidb --databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > ${BACKUP_TARGET}
     set +x
 
     echo "Done."
