160bb1e59904dc89b52da5af0efddcb6c24e4855
Charles Reid <charlesreid1@gmail.com>
2021-09-07 11:47:58 -0700

    update apply_templates to map all jinja vars to env vars

--- apply_templates.py
+++ apply_templates.py
@@ -1,4 +1,6 @@
-import os, re, sys
+import os
+import re
+import sys
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 """
@@ -28,9 +30,24 @@ tfile = 'docker-compose.yml.j2'
 rfile = 'docker-compose.yml'
 
 content = env.get_template(tfile).render({
-    "server_name_default" : "charlesreid1.com",
-    "mediawiki_secretkey" : "asdfqwerty_oiuqoweiruoasdfi",
-    "mysql_password" : "MySuperSecretPassword"
+    "pod_install_dir": os.environ('POD_CHARLESREID1_DIR'),
+    "top_domain": os.environ('POD_CHARLESREID1_TLD'),
+    "server_name_default" : os.environ('POD_CHARLESREID1_TLD'),
+    "username": os.environ('POD_CHARLESREID1_USER'),
+    # docker-compose:
+    "mysql_password" : os.environ('POD_CHARLESREID1_MYSQL_PASSWORD'),
+    "mediawiki_secretkey" : os.environ('POD_CHARLESREID1_MW_ADMIN_EMAIL'),
+    # mediawiki:
+    "admin_email": os.environ('POD_CHARLESREID1_MW_ADMIN_EMAIL'),
+    # gitea:
+    "gitea_app_name": os.environ('POD_CHARLESREID1_GITEA_APP_NAME'),
+    "gitea_secret_key": os.environ('POD_CHARLESREID1_GITEA_SECRET_KEY'),
+    "gitea_internal_token": os.environ('POD_CHARLESREID1_GITEA_INTERNAL_TOKEN'),
+    # aws:
+    "aws_backup_s3_bucket": os.environ('POD_CHARLESREID1_BACKUP_S3BUCKET'),
+    "aws_access_key": os.environ('POD_CHARLESREID1_AWS_ACCESS_KEY'),
+    "aws_access_secret": os.environ('POD_CHARLESREID1_AWS_ACCESS_SECRET'),
+    "backup_canary_webhook_url": os.environ('POD_CHARLESREID1_BACKUPCANARY_WEBHOOKURL'),
 })
 
 # Write to file
