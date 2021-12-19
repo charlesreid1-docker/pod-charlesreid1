ffc4f1d0c0c406761a242786827ae0f716e197c7
Charles Reid <charlesreid1@gmail.com>
2021-12-19 10:48:40 -0800

    add --no-progress flag to aws bacup script

--- scripts/backups/aws_backup.sh
+++ scripts/backups/aws_backup.sh
@@ -52,7 +52,7 @@ if [ "$#" == "0" ]; then
 
     # Copy to AWS
     echo "Backing up directory ${POD_CHARLESREID1_BACKUP_DIR}/${LAST_BACKUP}"
-    aws s3 cp --only-show-errors --recursive ${POD_CHARLESREID1_BACKUP_DIR}/${LAST_BACKUP} s3://${POD_CHARLESREID1_BACKUP_S3BUCKET}/backups/${LAST_BACKUP}
+    aws s3 cp --only-show-errors --no-progress --recursive ${POD_CHARLESREID1_BACKUP_DIR}/${LAST_BACKUP} s3://${POD_CHARLESREID1_BACKUP_S3BUCKET}/backups/${LAST_BACKUP}
     echo "Done."
 
 else
